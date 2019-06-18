2e700bf3a098b7437ade9c469a1c2f863dcda542
Lucie Leygue <Lucieleygue@MacBook-Air-de-Lucie.local>
2019-06-18 16:56:35 +0200

    tp3-1

---
+++ 3/script.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+ 	alert('Salut et bienvenue sur 1 jour, 1 challenge. Alors retrousse tes manches et on est ti-par !')
+	function welcome() {
+   	$("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+	}; 
+	welcome();
+ })
