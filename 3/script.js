44b43f074e4dd4ca1b88953655d475198bbe4529
Lucie Leygue <Lucieleygue@macbook-air-de-lucie.home>
2019-06-18 22:00:03 +0200

    tp3-2

--- 3/script.js
+++ 3/script.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
- 	alert('Salut et bienvenue sur 1 jour, 1 challenge. Alors retrousse tes manches et on est ti-par !')
+ 	alert('Salut et bienvenue sur 1 jour, 1 challenge.\n Alors retrousse tes manches et on est ti-par !')
 	function welcome() {
    	$("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
 	}; 
