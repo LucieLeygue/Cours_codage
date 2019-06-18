c66f8d25a53658f50b5bbacb041ba09f2c75fbec
Lucie Leygue <Lucieleygue@macbook-air-de-lucie.home>
2019-06-18 23:55:01 +0200

    tp3-3

--- 3/script.js
+++ 3/script.js
@@ -1,7 +1,22 @@
 $( document ).ready(function() {
- 	alert('Salut et bienvenue sur 1 jour, 1 challenge.\n Alors retrousse tes manches et on est ti-par !')
+ 	
+ 	alert('Prêt à relever un défi tous les jours ?\n Alors on est ti-par !')
+	
 	function welcome() {
-   	$("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+   	$("#welcome").html('<h4>Bienvenue, <br> choisis ton défi et impressionne-nous !</h4>');
 	}; 
-	welcome();
- })
+
+	function getUsername() {
+    //affiche une boite de dialogue 
+    var  user = prompt("Mais pour commencer, donne-nous ton petit nom ?", "");
+    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    return user
+	}; 
+
+	function welcome() {
+    var name= getUsername();
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+	};
+welcome();
+
+});
