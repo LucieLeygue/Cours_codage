43421f6fe5a34d35ac711c9f71157e3444a7e7df
Lucie Leygue <Lucieleygue@MacBook-Air-de-Lucie.local>
2019-06-18 15:55:11 +0200

    algo_java

--- 2/script.js
+++ 2/script.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
-  alert('Hello')
-  $("p").on( "click", function() {
-          $("p").addClass('red');
-    });
+  
+	alert('Hello')
+  
+	$("p").on("click",function() {
+
+	if ($("p").hasClass('red')) {
+    		$("p").removeClass('red');
+    	}else{
+    		$("p").addClass('red');
+    	};
+
+	});
+
 });
